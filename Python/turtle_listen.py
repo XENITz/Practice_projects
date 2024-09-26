b4e2e780f3bd7d3dd2977fd41f77843402788a8d
Imanol <90155626+XENITz@users.noreply.github.com>
2024-09-26 13:08:10 -0600

    Turtle race added

---
+++ Python/turtle_listen.py
@@ -0,0 +1,30 @@
+from turtle import  Turtle, Screen
+
+tim = Turtle()
+Screen = Screen()
+
+def move_forwards():
+    tim.forward(30)
+def move_backwards():
+    tim.backward(30)
+    
+def turn_left():    
+    tim.left(25)
+    
+def turn_right():
+    tim.right(25)
+    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+Screen.listen()
+Screen.onkey(key="w", fun=move_forwards)
+Screen.onkey(key="s", fun=move_backwards)
+Screen.onkey(key="a", fun=turn_left)
+Screen.onkey(key="d", fun=turn_right)
+Screen.onkey(key="c", fun=clear)
+
+Screen.exitonclick()
---
+++ turtle_race.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+screen.setup(width=1000, height=600)
+
+screen.exitonclick()
