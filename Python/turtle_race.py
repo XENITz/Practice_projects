a673e3efd39ebfb6e8a39c9af0dde7dfce3c2214
Imanol <90155626+XENITz@users.noreply.github.com>
2024-09-30 01:44:19 -0600

    Turtle race finished

--- Python/turtle_race.py
+++ Python/turtle_race.py
@@ -4,9 +4,10 @@ import random
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_positions = [-90, -60, -30, 0, 30, 60]
+colors = ["red", "orange", "black", "green", "blue", "purple"]
+y_positions = [-90, -60, -30, 90, 30, 60]
 all_turtles = []
+race_on = True
 
 
 for turtles_index in range(0, 6):
@@ -16,13 +17,18 @@ for turtles_index in range(0, 6):
     new_turtle.goto(x=-230, y=y_positions[turtles_index])
     all_turtles.append(new_turtle)
     
-while True:
+while race_on:
     for turtle in all_turtles:
+        if turtle.xcor() >= 100:
+            race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You won! The {winner} won the race")
+            else:
+                print(f"You lost! The {winner} won the race")
         steps = random.randint(0, 10)
         turtle.forward(steps)
-        if turtle.xcor() >= 100:
-            turtle.stamp()
-            break
+
 
 
 
