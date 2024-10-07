e5e86efc6fea89e2479ac99bf1724f44d612cfbf
Imanol <90155626+XENITz@users.noreply.github.com>
2024-10-07 13:46:37 -0600

    Added new class and fixed bugs in the movement

--- Python/snake_game/main.py
+++ Python/snake_game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 Screen = Screen()
 Screen.setup(width=600, height=600)
@@ -6,31 +7,18 @@ Screen.bgcolor("black")
 Screen.title("Snake Game")
 Screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-snakes_list = []
-
-##CREATE SNAKE
-for position in starting_positions:
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.penup()
-    snake.goto(position)
-    snakes_list.append(snake)
-
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     Screen.update()
     time.sleep(0.1)
     
-    for snake_number in range(len(snakes_list) - 1, 0, -1):
-        x = snakes_list[snake_number - 1].xcor()
-        y = snakes_list[snake_number - 1].ycor()
-        snake[snake_number].goto(x, y)
-    snake[0].forward(20)
+    snake.move()
+    
+    
 
-#
+
         
         
         
--- Python/snake_game/snake.py
+++ Python/snake_game/snake.py
@@ -1,7 +1,25 @@
 from turtle import Screen, Turtle
 import time
 
-
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 class Snake:
     def __init__(self):
-        self.
+        self.snakes_list = []
+        self.create_snake()
+        
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            snake = Turtle(shape="square")
+            snake.color("white")
+            snake.penup()
+            snake.goto(position)
+            self.snakes_list.append(snake)
+    
+    def move(self):
+        for snake_num in range(len(self.snakes_list) - 1, 0, -1):
+            x = self.snakes_list[snake_num - 1].xcor()
+            y = self.snakes_list[snake_num - 1].ycor()
+            self.snakes_list[snake_num].goto(x, y)
+
+        self.snakes_list[0].forward(MOVE_DISTANCE)
